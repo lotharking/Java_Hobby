d89612e56d0ede9a611c0d9c2a5e1473b067253c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-09-28 21:18:35 -0500

    async commit

--- Proyecto_Kafka/src/main/java/com/dev4j/kafka/consumer/Devs4jConsumer.java
+++ Proyecto_Kafka/src/main/java/com/dev4j/kafka/consumer/Devs4jConsumer.java
@@ -4,10 +4,10 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,15 +25,16 @@ public class Devs4jConsumer {
 		props.setProperty("auto.commit.interval.ms","1000");
 		props.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
 		props.setProperty("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+		props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
 		
 		try(KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)){
-			TopicPartition topicPartition = new TopicPartition("dev4j-topic", 4);
-			consumer.assign(Arrays.asList(topicPartition));
-			consumer.seek(topicPartition, 50);
+			consumer.subscribe(Arrays.asList("devs4j-topic"));
 			while(true) {
 				ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
 				for(ConsumerRecord<String, String> consumerRecord : consumerRecords) {
-					log.info("offset = {}, partition = {}, key = {}, value = {}", consumerRecord.offset(), consumerRecord.partition(), consumerRecord.key(), consumerRecord.value());
+					log.info("offset = {}, partition = {}, key = {}, value = {}", 
+							consumerRecord.offset(), consumerRecord.partition(), consumerRecord.key(), consumerRecord.value());
+					consumer.commitAsync();
 				}
 			}
 		}
