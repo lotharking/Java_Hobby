e82e4ed711316eacff306016062eaf50ecc23677
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-10-10 23:47:21 -0500

    Create consumer and producer in new project

--- kafka-spring/src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
+++ kafka-spring/src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
@@ -1,13 +1,36 @@
 package com.devs4j.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+public class KafkaSpringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	private static final Logger log = LoggerFactory.getLogger(KafkaSpringApplication.class);
 
+	@KafkaListener(topics ="devs4j-topic",containerFactory="listenerContainerFactory",groupId="devs4j-group3", 
+			properties ={"max.poll.interval.ms:60000","max.poll.records:100"})
+	public void listen(String message) {
+		log.info("Message received {} ", message);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		kafkaTemplate.send("devs4j-topic", "Sample Message ");
+		
 	}
 
 }
