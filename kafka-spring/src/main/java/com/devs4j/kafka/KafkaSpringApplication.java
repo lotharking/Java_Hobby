d1789813d107915102163554396f928df2ee3659
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-10-19 17:54:05 -0500

    add metrics for count message

--- kafka-spring/src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
+++ kafka-spring/src/main/java/com/devs4j/kafka/KafkaSpringApplication.java
@@ -19,39 +19,46 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.concurrent.ListenableFuture;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @SpringBootApplication
-public class KafkaSpringApplication implements CommandLineRunner{
+public class KafkaSpringApplication {
 	
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 	
+	@Autowired
+	private MeterRegistry meterRegistry;
+	
 	private static final Logger log = LoggerFactory.getLogger(KafkaSpringApplication.class);
 
-	@KafkaListener(id = "devs4jId", autoStartup = "false"
+	@KafkaListener(id = "devs4jId", autoStartup = "true"
 			, topics ="devs4j-topic",containerFactory="listenerContainerFactory",groupId="devs4j", 
-			properties ={"max.poll.interval.ms:4000","max.poll.records:10"})
+			properties ={"max.poll.interval.ms:4000","max.poll.records:50"})
 	public void listen(List<ConsumerRecord<String, String>> messages) {
-		log.info("Start reading messages...");
+		log.info("Message receive {} ", messages.size());
+		//log.info("Start reading messages...");
 		for(ConsumerRecord<String, String> message : messages) {
-			log.info("Partition = {}, Offset = {}, Key = {}, Value = {} ", message.partition(),message.offset(),message.key(),message.value());			
+			//log.info("Partition = {}, Offset = {}, Key = {}, Value = {} ", message.partition(),message.offset(),message.key(),message.value());			
 		}
-		log.info("Process complete.");
+		//log.info("Batch complete.");
 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringApplication.class, args);
 	}
 	
-	@Scheduled(fixedDelay = 1000, initialDelay = 5000)
-	public void print() {
-		log.info("Devs4j rocks!");
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		for(int i=0;i<100;i++) {
-			kafkaTemplate.send("devs4j-topic", String.valueOf(i), String.format("Sample message %d", i));			
+	@Scheduled(fixedDelay = 2000, initialDelay = 100)
+	public void SendKafkaMessages() {
+		for(int i=0; i < 200; i++) {
+			kafkaTemplate.send("devs4j-topic", String.valueOf(i), String.format("Sample message %d", i));
 		}
+	}
+	
+	@Scheduled(fixedDelay = 2000, initialDelay = 500)
+	public void printMetics() {
+		double count = meterRegistry.get("kafka.producer.record.send.total").functionCounter().count();
+		log.info("Count {} ", count);
 	}
 
 }
