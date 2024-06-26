37ac6bea597e8e8771b34ce1125331596bfe54f5
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-06-26 00:45:28 -0500

    fix: three sums v2

--- challenges/threeSums.java
+++ challenges/threeSums.java
@@ -1,15 +1,16 @@
 public class threeSums {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
         List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             list.add(nums[i]);
             result = secondValue(nums, list, i+1, result);
             list.remove(list.size()-1);
         }
-        return result;
+        return new ArrayList(result);
     }
-    public List<List<Integer>> secondValue(int[] nums, List<Integer> list, int index, List<List<Integer>> result){
+    public Set<List<Integer>> secondValue(int[] nums, List<Integer> list, int index, Set<List<Integer>> result){
         for(int i=index;i<nums.length;i++){
             list.add(nums[i]);
             result = lastValue(nums, list, i+1, result);
@@ -17,22 +18,13 @@ public class threeSums {
         }
         return result;
     }
-    public List<List<Integer>> lastValue(int[] nums, List<Integer> list, int index, List<List<Integer>> result){
+    public Set<List<Integer>> lastValue(int[] nums, List<Integer> list, int index, Set<List<Integer>> result){
         for(int i=index;i<nums.length;i++){
             list.add(nums[i]);
             int sum = list.stream().mapToInt(Integer::intValue).sum();
-            if(sum==0 && !result.contains(list) && isValidList(result, list)) result.add(new ArrayList<>(list));
+            if(sum==0) result.add(new ArrayList<>(list));
             list.remove(list.size()-1);
         }
         return result;
-    }
-    public boolean isValidList(List<List<Integer>> result, List<Integer> list){
-        List<Integer> lsort = new ArrayList<>(list); 
-        Collections.sort(lsort);
-        for(List<Integer> l: result){
-            Collections.sort(l);
-            if(l.equals(lsort)) return false;            
-        }
-        return true;
     }
 }
